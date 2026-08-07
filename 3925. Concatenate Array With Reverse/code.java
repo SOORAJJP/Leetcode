57391aa3e7d6e18033f67729c135ac8d00ddbe26
SOORAJJP <soorajjeyan@gmail.com>
2026-08-07 11:40:20 +0530

    Implement method to concatenate array with its reverse

---
+++ 3925. Concatenate Array With Reverse/code.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] concatWithReverse(int[] nums) {
+        int n = nums.length;
+        int[] arr = new int[n * 2];
+        for (int i = 0; i < n; i++) {
+            arr[i] = nums[i];         
+            arr[n + i] = nums[n - 1 - i];  
+        }
+        
+        return arr;
+    }
+}
